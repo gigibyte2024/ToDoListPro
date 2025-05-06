af2e25b02b62767ea4fd2e4a4628d3281f3b4d8c
Gargi Srivastava <gargisrivastava@Gargis-MacBook-Pro.local>
2025-05-06 16:42:48 +0530

    evalu

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,5 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar'; 
-// import LoginPage from './components/Login'; 
-// import Home from './components/Home';
-// import Main from './components/MainFile';
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Navbar />} />
-//         <Route path="/login" element={<LoginPage />} />
-//         <Route path="/home" element={<Home/>}/>
-//         <Route path= "/main" element={<Main/>}/>
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar'; 
 import LoginPage from './components/Login'; 
-import Home from './components/Home';
 import Main from './components/MainFile';
 
 function App() {
@@ -30,8 +7,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />         {/* Login page comes first */}
-        <Route path="/login" element={<LoginPage />} />    {/* Optional route for login */}
-        <Route path="/home" element={<Home />} />
         <Route path="/main" element={<Main />} />
       </Routes>
     </Router>
